53743e09ff9ae406b031f5d66c50580f8a86ca64
babourial <badreddine.abourial@cegedim.com>
2024-03-24 00:28:10 +0000

    adding connection to a mongodb databse

--- venv/AddDatatoDatabase.py
+++ venv/AddDatatoDatabase.py
@@ -1,46 +1,42 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from pymongo import MongoClient
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': ""
-})
+# MongoDB connection
+client = MongoClient('mongodb+srv://developerbadreddine:zt23FgyjykIYOW8J@cluster0.ns7t9lp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['smart_classroom_db']
+collection = db['attendance_records']
 
-ref = db.reference('Students')
+data = [
+    {
+        "_id": "321654",
+        "name": "Murtaza Hassan",
+        "major": "Robotics",
+        "starting_year": 2017,
+        "total_attendance": 7,
+        "standing": "G",
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    {
+        "_id": "852741",
+        "name": "Emly Blunt",
+        "major": "Economics",
+        "starting_year": 2021,
+        "total_attendance": 12,
+        "standing": "B",
+        "year": 1,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    {
+        "_id": "963852",
+        "name": "Elon Musk",
+        "major": "Physics",
+        "starting_year": 2020,
+        "total_attendance": 7,
+        "standing": "G",
+        "year": 2,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    }
+]
 
-data = {
-    "321654":
-        {
-            "name": "Murtaza Hassan",
-            "major": "Robotics",
-            "starting_year": 2017,
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "852741":
-        {
-            "name": "Emly Blunt",
-            "major": "Economics",
-            "starting_year": 2021,
-            "total_attendance": 12,
-            "standing": "B",
-            "year": 1,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "963852":
-        {
-            "name": "Elon Musk",
-            "major": "Physics",
-            "starting_year": 2020,
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 2,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        }
-}
-
-for key, value in data.items():
-    ref.child(key).set(value)
+# Insert data into the MongoDB collection
+collection.insert_many(data)
--- venv/main.py
+++ venv/main.py
@@ -4,18 +4,13 @@ import numpy as np
 import cv2
 import face_recognition
 import cvzone
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import storage
-import numpy as np
+from pymongo import MongoClient
 from datetime import datetime
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': "",
-    'storageBucket': ""
-})
+# MongoDB connection
+client = MongoClient('mongodb+srv://developerbadreddine:zt23FgyjykIYOW8J@cluster0.ns7t9lp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['smart_classroom_db']
+collection = db['attendance_records']
 
 bucket = storage.bucket()
 
